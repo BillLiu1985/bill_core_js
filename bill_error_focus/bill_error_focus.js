8055d65d7c7e25f596c854b35a9a434a56eed2f1
BILL LIU <o7z3149o0@hotmail.com>
2022-04-21 23:46:33 +0800

    3.4.3

--- bill_error_focus/bill_error_focus.js
+++ bill_error_focus/bill_error_focus.js
@@ -33,6 +33,31 @@
 				jQuery(jquery_expression).find(this.input_jquery_expression).removeClass("input_validation-failed");
 			}
 		},
+		'v2_attach':function(jquery_expression){
+			if(bill_core.global_typeof(jquery_expression)!=='string'){
+				bill_core.debug_console('jquery_expression error','error')
+				return;
+			}
+			this.v2_deattach(jquery_expression);
+			if(jQuery.bill_error_focus.action==='add_class'){
+				jQuery(jquery_expression).filter(this.input_jquery_expression).each(
+					function(){
+						if(jQuery(this).attr('validate_fail_message')!==''){
+							jQuery(this).addClass("input_validation-failed");
+						}
+					}
+				);
+			}
+		},
+		'v2_deattach':function(jquery_expression){
+			if(bill_core.global_typeof(jquery_expression)!=='string'){
+				bill_core.debug_console('jquery_expression error','error')
+				return;
+			}
+			if(jQuery.bill_error_focus.action==='add_class'){
+				jQuery(jquery_expression).filter(this.input_jquery_expression).removeClass("input_validation-failed");
+			}
+		},
 		'easy_attach':function(jquery_expression){
 			if(bill_core.global_typeof(jquery_expression)!=='string'){
 				bill_core.debug_console('jquery_expression error','error')
